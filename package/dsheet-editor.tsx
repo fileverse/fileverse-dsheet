f4898595b07cd3472f0e636b8577861b9a6ab090
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2025-06-08 07:40:38 +0530

    minor changes

--- package/dsheet-editor.tsx
+++ package/dsheet-editor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import cn from 'classnames';
 
 import { useFortuneDocumentStyle } from './hooks/use-document-style';
