76ece13cc6bfe93a696f27ccbcad35bf676c73f9
Kyle Nguyen <kyle.nguyen304@gmail.com>
2025-06-25 14:59:31 +0700

    @fileverse-dev/dsheet@1.0.30

--- package/components/editor-workbook.tsx
+++ package/components/editor-workbook.tsx
@@ -118,6 +118,7 @@ export const EditorWorkbook: React.FC<EditorWorkbookProps> = ({
     return (
       // @ts-ignore
       <Workbook
+        isFlvReadOnly={isReadOnly}
         isAuthorized={isAuthorized}
         key={workbookKey}
         ref={sheetEditorRef}
