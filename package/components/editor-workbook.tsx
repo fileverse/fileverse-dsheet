5d3a3c74529d62f8439710d7a70a0451d7ab68f6
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2025-07-14 19:24:33 +0530

    refactor deno code

--- package/components/editor-workbook.tsx
+++ package/components/editor-workbook.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Workbook } from '@fileverse-dev/fortune-react';
 import { Cell } from '@fileverse-dev/fortune-core';
 
@@ -17,6 +17,7 @@ import { handleExportToXLSX } from '../utils/xlsx-export';
 import { handleExportToCSV } from '../utils/csv-export';
 import { handleExportToJSON } from '../utils/json-export';
 import { useXLSXImport } from '../hooks/use-xlsx-import';
+import { useRefreshDenomination } from '../hooks/useRefreshDenomination';
 import { OnboardingHandlerType, DataBlockApiKeyHandlerType } from '../types';
 
 // Use the types defined in types.ts
@@ -109,66 +110,7 @@ export const EditorWorkbook: React.FC<EditorWorkbookProps> = ({
       : ['filter']
     : TOOL_BAR_ITEMS;
 
-  const cryptoPriceRef = useRef<{ ETH: number | null, BTC: number | null, SOL: number | null }>({ BTC: null, ETH: 0, SOL: 0 });
-  const intervalRef = useRef<NodeJS.Timeout | null>(null);
-
-  const fetchPrice = async () => {
-    // @ts-expect-error later
-    const cryptoPrices = await fetch(`${window.NEXT_PUBLIC_PROXY_BASE_URL}/proxy`, {
-      headers:
-      {
-        'target-url': 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd',
-        method: 'GET',
-        'Content-Type': 'application/json'
-      }
-    });
-    const cryptoData = await cryptoPrices.json();
-    const ETH = cryptoData.ethereum.usd;
-    const BTC = cryptoData.bitcoin.usd;
-    const SOL = cryptoData.solana.usd;
-    cryptoPriceRef.current = {
-      ETH,
-      BTC,
-      SOL
-    }
-    refreshDenomination();
-  }
-
-  const refreshDenomination = () => {
-    const { ETH, BTC, SOL } = cryptoPriceRef.current;
-    if (BTC === null || ETH === null || SOL === null) return;
-    const currentData = sheetEditorRef.current?.getSheet();
-    const cellData = currentData?.celldata;
-    if (!cellData) return;
-
-    for (let i = 0; i < cellData?.length; i++) {
-      const cell = { ...cellData[i] } as any; cellData[i];
-      cell.v = typeof cell.v === 'string' ? cell.v : { ...cellData[i].v };
-
-      const value = cell.v?.m?.toString();
-      const decemialCount = cell.v?.m?.includes('.') ? cell.v?.m?.split(' ')[0].split('.')[1].length : 0;
-      if (value?.includes("BTC")) {
-        cell.v.m = value.replace(/\d+(\.\d+)?/, (cell.v?.baseValue / BTC).toFixed(decemialCount).toString());
-      } else if (value?.includes("ETH")) {
-        cell.v.m = value.replace(/\d+(\.\d+)?/, (cell.v?.baseValue / ETH).toFixed(decemialCount).toString());
-      } else if (value?.includes("SOL")) {
-        cell.v.m = value.replace(/\d+(\.\d+)?/, (cell.v?.baseValue / SOL).toFixed(decemialCount).toString());
-      }
-
-      sheetEditorRef.current?.setCellValue(cell.r, cell.c, cell.v);
-    }
-  }
-
-  useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      fetchPrice();
-    }, 20 * 60 * 1000);
-
-    return () => {
-      if (intervalRef.current)
-        clearInterval(intervalRef.current);
-    };
-  }, []);
+  const { refreshDenomination } = useRefreshDenomination({ sheetEditorRef })
 
   // Memoized workbook component to avoid unnecessary re-renders
   return useMemo(() => {
