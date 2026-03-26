64e1d8274f4f292b2648eb15482d4434db1a9452
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2026-03-26 17:39:49 +0530

    fix cttrl + z for fx bar cal input

--- package/utils/diff-sheet.ts
+++ package/utils/diff-sheet.ts
@@ -35,8 +35,16 @@ export function diffObjectArrays<T extends Record<string, any>>(
   const oldMap = new Map<string, T>();
   const newMap = new Map<string, T>();
 
-  oldArr?.forEach((item, index) => oldMap.set(getKey ? getKey(item) : index, item));
-  newArr?.forEach((item, index) => newMap.set(getKey ? getKey(item) : index, item));
+  oldArr?.forEach((item, index) => {
+    console.log('item', item);
+    if (item)
+      oldMap.set(getKey ? getKey(item) : index, item)
+  });
+  newArr?.forEach((item, index) => {
+    console.log('item', item);
+    if (item)
+      newMap.set(getKey ? getKey(item) : index, item)
+  });
 
   const added: T[] = [];
   const removed: T[] = [];
