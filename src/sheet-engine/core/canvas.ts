3e9b039b9769e476ca6df59c965b8c31d75d3960
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2026-04-06 15:30:32 +0530

    fix: number overflow by default

--- src/sheet-engine/core/canvas.ts
+++ src/sheet-engine/core/canvas.ts
@@ -1329,7 +1329,7 @@ export class Canvas {
             if (mergeCell) {
               const mergeCellOffset =
                 borderOffset[
-                  `${bdRow + mergeCell.rs - 1}_${bdCol + mergeCell.cs - 1}`
+                `${bdRow + mergeCell.rs - 1}_${bdCol + mergeCell.cs - 1}`
                 ];
               mergeCellEndX = mergeCellOffset.endX;
               mergeCellEndY = mergeCellOffset.endY;
@@ -1491,19 +1491,26 @@ export class Canvas {
 
           const startX = c - 1 < 0 ? 0 : this.sheetCtx.visibledatacolumn[c - 1];
           const endX = this.sheetCtx.visibledatacolumn[c];
+          const hasExplicitHorizonAlign = !_.isNil(cell?.ht);
+          const effectiveHorizonAlign =
+            !hasExplicitHorizonAlign &&
+            horizonAlign === '2' &&
+            endX - startX < textMetrics
+              ? '1'
+              : horizonAlign;
 
           let stc;
           let edc;
 
           if (endX - startX < textMetrics) {
-            if (horizonAlign === '0') {
+            if (effectiveHorizonAlign === '0') {
               // 居中对齐
               const trace_forward = this.cellOverflow_trace(
                 r,
                 c,
                 c - 1,
                 'forward',
-                horizonAlign,
+                effectiveHorizonAlign,
                 textMetrics,
               );
               const trace_backward = this.cellOverflow_trace(
@@ -1511,7 +1518,7 @@ export class Canvas {
                 c,
                 c + 1,
                 'backward',
-                horizonAlign,
+                effectiveHorizonAlign,
                 textMetrics,
               );
 
@@ -1526,14 +1533,14 @@ export class Canvas {
               } else {
                 edc = trace_backward.c - 1;
               }
-            } else if (horizonAlign === '1') {
+            } else if (effectiveHorizonAlign === '1') {
               // 左对齐
               const trace = this.cellOverflow_trace(
                 r,
                 c,
                 c + 1,
                 'backward',
-                horizonAlign,
+                effectiveHorizonAlign,
                 textMetrics,
               );
               stc = c;
@@ -1543,14 +1550,14 @@ export class Canvas {
               } else {
                 edc = trace.c - 1;
               }
-            } else if (horizonAlign === '2') {
+            } else if (effectiveHorizonAlign === '2') {
               // 右对齐
               const trace = this.cellOverflow_trace(
                 r,
                 c,
                 c - 1,
                 'forward',
-                horizonAlign,
+                effectiveHorizonAlign,
                 textMetrics,
               );
               edc = c;
@@ -2367,19 +2374,20 @@ export class Canvas {
 
       const textInfo = cell
         ? getCellTextInfo(
-            cell,
-            renderCtx,
-            this.sheetCtx,
-            {
-              cellWidth,
-              cellHeight,
-              space_width,
-              space_height,
-              r,
-              c,
-            },
-            this.sheetCtx,
-          )
+          cell,
+          renderCtx,
+          this.sheetCtx,
+          {
+            cellWidth,
+            originCellWidth: cellWidth,
+            cellHeight,
+            space_width,
+            space_height,
+            r,
+            c,
+          },
+          this.sheetCtx,
+        )
         : undefined;
 
       // 若单元格有条件格式图标集
@@ -2600,6 +2608,13 @@ export class Canvas {
     }
 
     const endX = this.sheetCtx.visibledatacolumn[edc] - scrollWidth;
+    let sourceStartX;
+    if (c === 0) {
+      sourceStartX = -scrollWidth;
+    } else {
+      sourceStartX = this.sheetCtx.visibledatacolumn[c - 1] - scrollWidth;
+    }
+    const sourceEndX = this.sheetCtx.visibledatacolumn[c] - scrollWidth;
 
     //
     const flowdata = getFlowdata(this.sheetCtx);
@@ -2608,6 +2623,7 @@ export class Canvas {
     }
     const cell = flowdata[r][c];
     const cellWidth = endX - startX - 2;
+    const originCellWidth = sourceEndX - sourceStartX - 2;
     const cellHeight = endY - startY - 2;
     const space_width = 2;
     const space_height = 2; // 宽高方向 间隙
@@ -2630,19 +2646,20 @@ export class Canvas {
 
     const textInfo = cell
       ? getCellTextInfo(
-          cell,
-          renderCtx,
-          this.sheetCtx,
-          {
-            cellWidth,
-            cellHeight,
-            space_width,
-            space_height,
-            r,
-            c,
-          },
-          this.sheetCtx,
-        )
+        cell,
+        renderCtx,
+        this.sheetCtx,
+        {
+          cellWidth,
+          originCellWidth,
+          cellHeight,
+          space_width,
+          space_height,
+          r,
+          c,
+        },
+        this.sheetCtx,
+      )
       : undefined;
 
     // 交替颜色
