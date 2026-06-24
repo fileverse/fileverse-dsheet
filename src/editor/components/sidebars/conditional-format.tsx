e16c5c0257281d115a3c7960208db9889c0f4c07
Maitra Khatri <maitrakhatri@gmail.com>
2026-06-24 18:22:13 +0530

    feat(sidebar): data-verification + conditional-format panels

---
+++ src/editor/components/sidebars/conditional-format.tsx
@@ -0,0 +1,11 @@
+const ConditionalFormat = () => {
+  return (
+    <div
+      id="placeholder-conditional-format"
+      className="h-[calc(100vh-200px)] overflow-y-auto no-scrollbar"
+      style={{ padding: '16px' }}
+    ></div>
+  );
+};
+
+export { ConditionalFormat };
